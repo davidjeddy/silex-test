e2e85a49afaa3ca348c6c4e65b1a405f438a8a2d
David J Eddy <me@davidjeddy.com>
2016-12-29 11:22:26 -0500

    FIXED PDO driver, now able to get a basic response.

--- code/index.php
+++ code/index.php
@@ -5,39 +5,25 @@
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/common/env.php';
 
-use Symfony\Component\HttpFoundation\Request;
-
 $app = new Silex\Application();
-$app['debug'] = getenv('DEBUG');
-
-Request::enableHttpMethodParameterOverride();
+$app['debug'] = true;
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), [
     'db.options' => [
-        'mysql' => [
-            'driver' => getenv('DB_DRIVER'),
-            'dbname' => getenv('DB_NAME'),
-            'host' => getenv('DB_HOST'),
-            'user' => getenv('DB_USER'),
-            'password' => getenv('DB_PASS'),
-            'charset' => getenv('DB_CHAR'),
-            'port' => getenv('DB_PORT'),
-        ]
+        'driver' => 'pdo_mysql',
+        'dbname' => 'app_db',
+        'host' => '172.17.0.2',
+        'user' => 'app_db_user',
+        'password' => 'app_db_pass',
+        'charset' => 'utf8mb4',
+        'port' => '3306',
     ]
 ]);
 
-$app->match('/', function () use ($app) {
-    return 'Hello World';
-});
-
-$app->get('/hello/', function ($id = 1) use ($app) {
-    $sql = 'SELECT * FROM test WHERE id = ?';
-    $data = $app['db']->fetchAssoc($sql, array((int) $id));
+$app->get('/', function () use ($app) {
+    $sql = 'SELECT * FROM test WHERE id = 1';
+    $data = $app['db']->fetchAssoc($sql);
     return  json_encode($data);
-});
-
-$app->match('/hello/{name}', function($name) use($app) {
-    return 'Hello '.$app->escape($name);
 });
 
 $app->run();
