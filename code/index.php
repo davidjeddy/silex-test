694e49e9816989a5653599e9bd45d6439558eb10
David J Eddy <me@davidjeddy.com>
2016-12-30 11:15:11 -0500

    DB conn working. INIT 3 resources working.

--- code/index.php
+++ code/index.php
@@ -1,4 +1,9 @@
 <?php
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 # silex micro framework basic entry script
 
 // web/index.php
@@ -7,6 +12,7 @@ require_once __DIR__.'/common/env.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
+
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), [
     'db.options' => [
@@ -20,10 +26,21 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), [
     ]
 ]);
 
+# TODO find out why 'test' does not work as a route - DJE
+$app->get('/testing/{id}', function ($id) use ($app) {
+    $sql = 'SELECT * FROM test WHERE id = ?';
+    $data = $app['db']->fetchAssoc($sql, [$app->escape($id)]);
+    return $app->json($data);
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
 $app->get('/', function () use ($app) {
     $sql = 'SELECT * FROM test WHERE id = 1';
     $data = $app['db']->fetchAssoc($sql);
-    return  json_encode($data);
+    return $app->json($data);
 });
 
 $app->run();
