4e6b2d40353f84b8afea3d719abfe9342dcee006
David J Eddy <me@davidjeddy.com>
2016-12-30 12:29:50 -0500

    ADDED error handling example.

--- code/index.php
+++ code/index.php
@@ -40,6 +40,19 @@ $app['controllers']
     //->when('request.isSecure() == true')
 ;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+$app->error(function (\Exception $e, Request $request, $code) {
+    switch ($code) {
+        case 404:
+            $message = 'The requested page could not be found.';
+            break;
+        default:
+            $message = 'We are sorry, but something went terribly wrong.';
+    }
+    return new Response($message);
+});
+
 $app->get('/cntlConf', function() use ($app) {
     return $app->json(['message' => 'CNTL conf example.']);
 });
